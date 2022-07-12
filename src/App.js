be94e02f491b01dc00ab77ba8e70dd758b9b01b4
Proxypepe <sasha_golovin_95@bk.ru>
2022-07-13 01:30:07 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './styles/App.css';
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import {useFetching} from "./hooks/useFatching";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    })
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+        setModal(false);
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
+    return (
+        <div className="App">
+            <button onClick={fetchPosts}>
+                Get Posts
+            </button>
+
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Create post
+            </MyButton>
+
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+
+            <hr style={{margin: '15px'}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            { postError &&
+                <h1>Error happened ${postError}</h1>
+            }
+            {isPostLoading ?
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: 50
+                }}>
+                    <Loader/>
+                </div>
+                :
+                <PostList posts={sortedAndSearchedPosts} title='Post list' remove={removePost}/>
+            }
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
+const PostList = ({posts, title, remove }) => {
+    return (
+        <div>
+            <h1 style={{textAlign:'center'}}>
+                {title}
+            </h1>
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem number={index + 1} post={post}  remove={remove}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
+        </div>
+    );
+};
+
+
+export default PostList;
