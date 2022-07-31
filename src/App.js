45642a6a235d19cdb3d35438a53412d39f43c0ce
Proxypepe <sasha_golovin_95@bk.ru>
2022-07-31 21:30:42 +0300

    added pagination

--- src/App.js
+++ src/App.js
@@ -9,21 +9,33 @@ import {usePosts} from "./hooks/usePosts";
 import {useFetching} from "./hooks/useFatching";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import {getPageCount} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
+
 
 function App() {
 
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+
+
     const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll();
-        setPosts(posts);
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount, limit))
+
     })
 
     useEffect(() => {
         fetchPosts()
-    }, [])
+    }, [page])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -34,12 +46,12 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     }
 
+    const changePage = (page) => {
+        setPage(page);
+    }
+
     return (
         <div className="App">
-            <button onClick={fetchPosts}>
-                Get Posts
-            </button>
-
             <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
                 Create post
             </MyButton>
@@ -53,8 +65,8 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
-            { postError &&
-                <h1>Error happened ${postError}</h1>
+            {postError &&
+            <h1>Error happened ${postError}</h1>
             }
             {isPostLoading ?
                 <div style={{
@@ -67,8 +79,11 @@ function App() {
                 :
                 <PostList posts={sortedAndSearchedPosts} title='Post list' remove={removePost}/>
             }
-
-
+            <Pagination
+                page={page}
+                changePage={changePage}
+                totalPages={totalPages}
+            />
         </div>
     );
 }
